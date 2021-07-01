efea14a1e93b0e6820c3fc0435eb128456a7ac1d
kneeraazon01 <karkinirajan1999@gmail.com>
2021-07-01 10:39:45 +0545

    xuttai  virtual environment banako xa

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,11 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, UserModel
-
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UserModel
 
 UserModel = get_user_model()
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Account, Profile
 
